a2e2358a9f895bccbcc24019ca2278f90989f993
popopopoya <popopopoya81@gmail.com>
2025-09-10 11:54:16 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Label(text='hello')
+    
+MyApp().run()    
